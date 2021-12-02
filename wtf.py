4aae524b3111e9f5cc182b484ffff27d2a1baf27
Arseniy Simonov <catterer@yandex.ru>
2021-12-02 14:29:13 +0000

    fix

--- wtf.py
+++ wtf.py
@@ -6,7 +6,7 @@ N = 4
 for i in range(50000):
   a1 = random.randrange(N)
   a2 = random.randrange(N-1)
-  if a2 == a1:
+  if a2 >= a1:
     a2 += 1
   print("{}".format(a2))
   #print("{}".format(sorted([a1, a2])))
